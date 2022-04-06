e510a65cf91d5554ae6760e0c13fb02e51ab2e71
Arturo <a.reygadas.g@gmail.com>
2022-04-05 22:51:13 -0500

    sync with github

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import FormPage from './pages/signIn'
+import {home} from './pages/home'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <FormPage />
+
+      
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const BtnL = styled.button`
+    background: rgb(2,0,36);
+    background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(5,5,77,1) 38%, rgba(0,212,255,1) 100%);
+    color:#fff;
+    padding: 5px 15px 5px 15px;
+    border:none ;
+    border-radius: 15px ;
+    box-shadow: 7px 7px 7px rgba(0,0,0,0.8) ;
+    &:hover{
+    background: linear-gradient(29deg, rgba(2,0,36,1) 0%, rgba(5,5,77,1) 38%, rgba(0,212,255,1) 100%);
+}
+`;
+
+export const Card = styled.div`
+    margin-top: 5%;
+    box-shadow: 20px 20px 50px rgba(0,0,0,0.5);
+    padding: ;
+`;
